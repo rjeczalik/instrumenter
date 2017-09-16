7cd19280ce29e9f1dc73d9afc82f535d6730809b
Rafal Jeczalik <rjeczalik@gmail.com>
2017-09-16 05:52:18 +0200

    cmd/intercept: generate template assets

--- cmd/instrument/main.go
+++ cmd/instrument/main.go
@@ -1,4 +1,7 @@
 package main
 
+//go:generate go get github.com/jteeuwen/go-bindata/go-bindata golang.org/x/tools/cmd/eg golang.org/x/tools/cmd/goimports
+//go:generate go-bindata -nometadata -o builtin.go -prefix ../../templates/ ../../templates/
+
 func main() {
 }
