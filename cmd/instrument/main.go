fa15873b16c75641b1d2fe6ac1d3681ccc7c8442
Rafal Jeczalik <rjeczalik@gmail.com>
2017-09-16 07:00:38 +0200

    cmd/instrument: ensure modified eg is installed

--- cmd/instrument/main.go
+++ cmd/instrument/main.go
@@ -1,6 +1,7 @@
 package main
 
-//go:generate go get github.com/jteeuwen/go-bindata/go-bindata golang.org/x/tools/cmd/eg golang.org/x/tools/cmd/goimports
+//go:generate go get github.com/jteeuwen/go-bindata/go-bindata golang.org/x/tools/cmd/goimports
+//go:generate go install ../../vendor/golang.org/x/tools/cmd/eg
 //go:generate go-bindata -nometadata -o builtin.go -prefix ../../templates/ ../../templates/
 
 func main() {
